94c928bf7f6fcef7949987bba5e4ea86c8b36275
SecretTermite <61213486+SecretTermite@users.noreply.github.com>
2020-02-19 12:07:42 +0000

    Add files via upload

---
+++ loginScreen/loginScreen/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace loginScreen
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("data.txt")) 
+            {
+                
+            }
+
+
+                string passAttempt = txtPassword.Text;
+            string nameAttempt = txtUsername.Text;
+            if (password == passAttempt&& username == nameAttempt)
+            {
+                MessageBox.Show ("All Gucci");
+            }
+            
+            else if (password != passAttempt && username == nameAttempt)
+            {
+                MessageBox.Show("Incorrect Password");
+            }
+
+            else if (username != nameAttempt && nameAttempt != "")
+            {
+                MessageBox.Show("Username not found");
+            }
+
+        }
+    }
+}
---
+++ loginScreen/loginScreen/user.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loginScreen
+{
+    class user
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public Type UserType  { get; set; }
+
+        public enum Type
+        {
+            Admin = 0,
+            Worker = 1
+        }
+
+        bool checkPassword(string password)
+        {
+            if (password == this.Password)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
